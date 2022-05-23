61fcafc524ae3ec55d53c2b19e1478b70ccbf5a1
Jay Prakash Pathak <jayprakashpathak@Jays-MacBook-Air.local>
2022-05-24 01:21:21 +0530

    initial project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let Homepage : React.FC = () => {
+    return (
+        <div>
+            <h1>Our amazing awsome project</h1>
+        </div>
+    )
+}
+
+export default Homepage
